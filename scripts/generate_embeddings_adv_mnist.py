301f35630308eee17a150494962260dd54a9fa77
ruanvdmerwe <ruanvdmerwe@outlook.com>
2022-01-09 16:50:21 +0200

    updated scritps for result to be bit less run at once and to save to right location

--- scripts/generate_embeddings_adv_mnist.py
+++ scripts/generate_embeddings_adv_mnist.py
@@ -51,172 +51,173 @@ class QueryRefrenceImageEncoder(pl.LightningModule):
 
 if __name__ == "__main__":
 
-    train, test = load_mnist_dataset('./')
+    train, test = load_mnist_dataset('../')
 
-
-    encoder = LeNet(
-            embedding_dim=EMBEDDING_DIM, 
-            dropout=0,
-            logits=True,
-            number_classes=10
-        )
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=mnist/triplet-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_tripent = model.encoder
-    encoder_tripent.eval()
-    encoder_tripent.cuda()
-    encoder_tripent.logits=True
-
-
-
-    encoder = LeNet(
-            embedding_dim=EMBEDDING_DIM, 
-            dropout=0,
-            logits=True,
-            number_classes=10
-        )
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=mnist/cross-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_xent = model.encoder
-    encoder_xent.eval()
-    encoder_xent.cuda()
-    encoder_xent.logits=True
+    for OPTIM in ['adam', 'sgd']:
+        for EM_DIM in [256, 512, 1024,2048]:
+            encoder = LeNet(
+                    embedding_dim=EMBEDDING_DIM, 
+                    dropout=0,
+                    logits=True,
+                    number_classes=10
+                )
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=mnist/triplet-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_tripent = model.encoder
+            encoder_tripent.eval()
+            encoder_tripent.cuda()
+            encoder_tripent.logits=True
 
 
 
-    encoder = LeNet(
-            embedding_dim=EMBEDDING_DIM, 
-            dropout=0,
-            logits=False,
-            number_classes=None
-        )
+            encoder = LeNet(
+                    embedding_dim=EMBEDDING_DIM, 
+                    dropout=0,
+                    logits=True,
+                    number_classes=10
+                )
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=mnist/cross-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_xent = model.encoder
+            encoder_xent.eval()
+            encoder_xent.cuda()
+            encoder_xent.logits=True
 
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=mnist/nt-xent/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_ntxent = model.encoder
-    encoder_ntxent.eval()
-    encoder_ntxent.cuda()
-    encoder_ntxent.logits=False
 
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=mnist/triplet-supervised/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_trip_sup = model.encoder
-    encoder_trip_sup.eval()
-    encoder_trip_sup.cuda()
-    encoder_trip_sup.logits=False
 
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=mnist/triplet/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_trip = model.encoder
-    encoder_trip.eval()
-    encoder_trip.cuda()
-    encoder_trip.logits=False
+            encoder = LeNet(
+                    embedding_dim=EMBEDDING_DIM, 
+                    dropout=0,
+                    logits=False,
+                    number_classes=None
+                )
 
-    epsilons_dist = np.linspace(start=0, stop=0.2, num=100)
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=mnist/nt-xent/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_ntxent = model.encoder
+            encoder_ntxent.eval()
+            encoder_ntxent.cuda()
+            encoder_ntxent.logits=False
 
-    encoder_random = LeNet(
-            embedding_dim=EMBEDDING_DIM, 
-            dropout=0,
-            logits=False,
-            number_classes=None
-        )
-    # encoder_random.load_state_dict(torch.load(f'./multirun/mnist_encoder_random_dim{EMBEDDING_DIM}.pt')) #ensure random always the same
-    encoder_random.eval()
-    encoder_random.cuda()
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=mnist/triplet-supervised/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_trip_sup = model.encoder
+            encoder_trip_sup.eval()
+            encoder_trip_sup.cuda()
+            encoder_trip_sup.logits=False
 
-    models = [encoder_random, encoder_tripent, encoder_xent, encoder_ntxent, encoder_trip_sup, encoder_trip]
-    model_names = ['random_init', 'tripent_mnist', 'xent_mnist','ntxent_mnist', 'trip_sup_mnist', 'trip_mnist']
-    losses = {
-        'random_init': NtXentLoss(),
-        'tripent_mnist': TripletEntropyLoss(),
-        'xent_mnist': CrossEntropyLoss(),
-        'ntxent_mnist': NtXentLoss(),
-        'trip_sup_mnist': TripletLossSupervised(),
-        'trip_mnist': TripletLoss()
-    }
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=mnist/triplet/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_trip = model.encoder
+            encoder_trip.eval()
+            encoder_trip.cuda()
+            encoder_trip.logits=False
 
-    aug_transform = torchvision.transforms.Compose([
-        torchvision.transforms.RandomRotation(15),
-        torchvision.transforms.Normalize((0.1307,), (0.3081,))
-    ])
+            epsilons_dist = np.linspace(start=0, stop=0.2, num=100)
 
-    transform = torchvision.transforms.Normalize((0.1307,), (0.3081,))
-    dataloader = torch.utils.data.DataLoader(test, batch_size=128, shuffle=False, num_workers=2, persistent_workers=True, pin_memory=False, drop_last=False)
+            encoder_random = LeNet(
+                    embedding_dim=EMBEDDING_DIM, 
+                    dropout=0,
+                    logits=False,
+                    number_classes=None
+                )
+            # encoder_random.load_state_dict(torch.load(f'../multirun/mnist_encoder_random_dim{EMBEDDING_DIM}.pt')) #ensure random always the same
+            encoder_random.eval()
+            encoder_random.cuda()
 
-    df = pd.DataFrame()
+            models = [encoder_random, encoder_tripent, encoder_xent, encoder_ntxent, encoder_trip_sup, encoder_trip]
+            model_names = ['random_init', 'tripent_mnist', 'xent_mnist','ntxent_mnist', 'trip_sup_mnist', 'trip_mnist']
+            losses = {
+                'random_init': NtXentLoss(),
+                'tripent_mnist': TripletEntropyLoss(),
+                'xent_mnist': CrossEntropyLoss(),
+                'ntxent_mnist': NtXentLoss(),
+                'trip_sup_mnist': TripletLossSupervised(),
+                'trip_mnist': TripletLoss()
+            }
 
-    for model, name in tqdm(zip(models, model_names), total=len(models), desc='model', leave=False):
-        for iteration in tqdm(range(PGD_ITTERATION), desc='pgd iteration', leave=False):
-            projected_points = np.zeros(shape=(1, EMBEDDING_DIM))
-            labels = []
+            aug_transform = torchvision.transforms.Compose([
+                torchvision.transforms.RandomRotation(15),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,))
+            ])
 
-            for batch in tqdm(dataloader, desc='sample', leave=False):
-                images, labs = batch
-                labels.extend([int(l) for l in labs])
+            transform = torchvision.transforms.Normalize((0.1307,), (0.3081,))
+            dataloader = torch.utils.data.DataLoader(test, batch_size=128, shuffle=False, num_workers=2, persistent_workers=True, pin_memory=False, drop_last=False)
 
-                if name in ['random_init','ntxent_mnist', 'trip_mnist']:
-                    images = return_fgsm_contrastive_attack_images(
-                        images=images,
-                        model=model,
-                        loss_fn=losses[name],
-                        val_transform=transform,
-                        transform=aug_transform,
-                        iterations=iteration,
-                        epsilon=2/255 
-                    )
-                elif name in ['tripent_mnist', 'xent_mnist']:
-                    model.logits = True
-                    images = return_fgsm_supervised_attack_images(
-                        images=images,
-                        model=model,
-                        labels=labs,
-                        loss_fn=losses[name],
-                        final_transform=aug_transform,
-                        require_logits=True,
-                        iterations=iteration,
-                        epsilon=2/255 
-                    )
-                    model.logits = False
-                else:
-                    images = return_fgsm_supervised_attack_images(
-                        images=images,
-                        model=model,
-                        labels=labs,
-                        loss_fn=losses[name],
-                        final_transform=aug_transform,
-                        require_logits=False,
-                        iterations=iteration,
-                        epsilon=2/255 
-                    )
-                    model.logits = False
+            df = pd.DataFrame()
 
-                with torch.no_grad():
-                    
-                    reps = model(images.cuda()).cpu().numpy()
-                projected_points = np.concatenate((projected_points, reps))
+            for model, name in tqdm(zip(models, model_names), total=len(models), desc='model', leave=False):
+                for iteration in tqdm(range(PGD_ITTERATION), desc='pgd iteration', leave=False):
+                    projected_points = np.zeros(shape=(1, EMBEDDING_DIM))
+                    labels = []
 
-            projected_points = projected_points[1:]
-            _ = pd.DataFrame(projected_points)
-            _['pgd_iterations'] = iteration
-            _['model'] = name
-            _['image_index'] = list(range(len(labels)))
-            _['label'] = labels
-            df = pd.concat([df, _])
+                    for batch in tqdm(dataloader, desc='sample', leave=False):
+                        images, labs = batch
+                        labels.extend([int(l) for l in labs])
 
-        fcols = df.select_dtypes('float').columns
-        icols = df.select_dtypes('integer').columns
+                        if name in ['random_init','ntxent_mnist', 'trip_mnist']:
+                            images = return_fgsm_contrastive_attack_images(
+                                images=images,
+                                model=model,
+                                loss_fn=losses[name],
+                                val_transform=transform,
+                                transform=aug_transform,
+                                iterations=iteration,
+                                epsilon=2/255 
+                            )
+                        elif name in ['tripent_mnist', 'xent_mnist']:
+                            model.logits = True
+                            images = return_fgsm_supervised_attack_images(
+                                images=images,
+                                model=model,
+                                labels=labs,
+                                loss_fn=losses[name],
+                                final_transform=aug_transform,
+                                require_logits=True,
+                                iterations=iteration,
+                                epsilon=2/255 
+                            )
+                            model.logits = False
+                        else:
+                            images = return_fgsm_supervised_attack_images(
+                                images=images,
+                                model=model,
+                                labels=labs,
+                                loss_fn=losses[name],
+                                final_transform=aug_transform,
+                                require_logits=False,
+                                iterations=iteration,
+                                epsilon=2/255 
+                            )
+                            model.logits = False
 
-        df[fcols] = df[fcols].apply(pd.to_numeric, downcast='float')
-        df[icols] = df[icols].apply(pd.to_numeric, downcast='integer')
+                        with torch.no_grad():
+                            
+                            reps = model(images.cuda()).cpu().numpy()
+                        projected_points = np.concatenate((projected_points, reps))
 
-        save_loc = f'results/data=mnist/{OPTIM}/adverserial_attacks/embedding_dim={EMBEDDING_DIM}/'
-        if not os.path.exists(save_loc):
-            os.makedirs(save_loc)
-        df.to_pickle(f'{save_loc}/{name}_adverserial.pickle')
-        df = pd.DataFrame()
+                    projected_points = projected_points[1:]
+                    _ = pd.DataFrame(projected_points)
+                    _['pgd_iterations'] = iteration
+                    _['model'] = name
+                    _['image_index'] = list(range(len(labels)))
+                    _['label'] = labels
+                    df = pd.concat([df, _])
+
+                fcols = df.select_dtypes('float').columns
+                icols = df.select_dtypes('integer').columns
+
+                df[fcols] = df[fcols].apply(pd.to_numeric, downcast='float')
+                df[icols] = df[icols].apply(pd.to_numeric, downcast='integer')
+
+                save_loc = f'../results/data=mnist/{OPTIM}/adverserial_attacks/embedding_dim={EMBEDDING_DIM}/'
+                if not os.path.exists(save_loc):
+                    os.makedirs(save_loc)
+                df.to_pickle(f'{save_loc}/{name}_adverserial.pickle')
+                df = pd.DataFrame()
         
--- scripts/generate_embeddings_cifar.py
+++ scripts/generate_embeddings_cifar.py
@@ -49,128 +49,129 @@ class QueryRefrenceImageEncoder(pl.LightningModule):
 
 if __name__ == "__main__":
 
-    train, test = load_cifar10_dataset('./')
+    train, test = load_cifar10_dataset('../')
 
+    for OPTIM in ['sgd', 'adam']:
+        for EMBEDDING_DIM in [128]:
 
-    encoder = CifarResNet18(
-            embedding_dim=EMBEDDING_DIM, 
-            hidden_dim=1024,
-            logits=True,
-            number_classes=10
-        )
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=cifar10/triplet-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_tripent = model.encoder
-    encoder_tripent.eval()
-    encoder_tripent.cuda()
-    encoder_tripent.logits=False
-
-
-
-    encoder = CifarResNet18(
-            embedding_dim=EMBEDDING_DIM, 
-            hidden_dim=1024,
-            logits=True,
-            number_classes=10
-        )
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=cifar10/cross-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_xent = model.encoder
-    encoder_xent.eval()
-    encoder_xent.cuda()
-    encoder_xent.logits=False
+            encoder = CifarResNet18(
+                    embedding_dim=EMBEDDING_DIM, 
+                    hidden_dim=1024,
+                    logits=True,
+                    number_classes=10
+                )
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=cifar10/triplet-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_tripent = model.encoder
+            encoder_tripent.eval()
+            encoder_tripent.cuda()
+            encoder_tripent.logits=False
 
 
 
-    encoder = CifarResNet18(
-            embedding_dim=EMBEDDING_DIM, 
-            hidden_dim=1024,
-            logits=False,
-            number_classes=None
-        )
-
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=cifar10/nt-xent/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_ntxent = model.encoder
-    encoder_ntxent.eval()
-    encoder_ntxent.cuda()
-    encoder_ntxent.logits=False
-
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=cifar10/triplet-supervised/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_trip_sup = model.encoder
-    encoder_trip_sup.eval()
-    encoder_trip_sup.cuda()
-    encoder_trip_sup.logits=False
-
-    model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
-    model = model.load_from_checkpoint(f'./multirun/data=cifar10/triplet/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
-    model.eval()
-    encoder_trip = model.encoder
-    encoder_trip.eval()
-    encoder_trip.cuda()
-    encoder_trip.logits=False
-
-
-    epsilons_dist = np.linspace(start=0, stop=1.0, num=100)
-
-    encoder_random = CifarResNet18(
-            embedding_dim=EMBEDDING_DIM, 
-            hidden_dim=1024,
-            logits=False,
-            number_classes=None
-        )
-    encoder_random.load_state_dict(torch.load(f'./multirun/cifar_encoder_random_dim{EMBEDDING_DIM}.pt')) #ensure random always the same
-    encoder_random.eval()
-    encoder_random.cuda()
-
-    models = [encoder_random, encoder_tripent, encoder_xent,
-            encoder_ntxent, encoder_trip_sup, encoder_trip]
-    model_names = ['random_init', 'tripent_cifar10', 'xent_cifar10','ntxent_cifar10', 'trip_sup_cifar10', 'trip_cifar10']
+            encoder = CifarResNet18(
+                    embedding_dim=EMBEDDING_DIM, 
+                    hidden_dim=1024,
+                    logits=True,
+                    number_classes=10
+                )
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=cifar10/cross-entropy/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_xent = model.encoder
+            encoder_xent.eval()
+            encoder_xent.cuda()
+            encoder_xent.logits=False
 
 
 
-    transform = torchvision.transforms.Normalize([0.49139968, 0.48215827 ,0.44653124], [0.24703233, 0.24348505, 0.26158768])
-    dataloader = torch.utils.data.DataLoader(test, batch_size=1024, shuffle=False, num_workers=8, persistent_workers=True, pin_memory=False, drop_last=False)
+            encoder = CifarResNet18(
+                    embedding_dim=EMBEDDING_DIM, 
+                    hidden_dim=1024,
+                    logits=False,
+                    number_classes=None
+                )
 
-    df = pd.DataFrame()
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=cifar10/nt-xent/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_ntxent = model.encoder
+            encoder_ntxent.eval()
+            encoder_ntxent.cuda()
+            encoder_ntxent.logits=False
 
-    for confidence_run in tqdm(range(0,3), desc='confidence_run'):
-        for model, name in tqdm(zip(models, model_names), total=len(models), desc='model', leave=False):
-            for epsilon in tqdm(epsilons_dist, desc='epsilon', leave=False):
-                projected_points = np.zeros(shape=(1, EMBEDDING_DIM))
-                labels = []
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=cifar10/triplet-supervised/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_trip_sup = model.encoder
+            encoder_trip_sup.eval()
+            encoder_trip_sup.cuda()
+            encoder_trip_sup.logits=False
 
-                for batch in tqdm(dataloader, desc='sample', leave=False):
-                    images, labs = batch
-                    labels.extend([int(l) for l in labs])
-                    images = transform(torch.clip(images + torch.normal(0, epsilon, size=images.shape), 0, 1))
+            model = QueryRefrenceImageEncoder(encoder=encoder,loss_fn=None,optim_cfg=None,)
+            model = model.load_from_checkpoint(f'../multirun/data=cifar10/triplet/{OPTIM}/encoder.embedding_dim={EMBEDDING_DIM}/checkpoints/last.ckpt')
+            model.eval()
+            encoder_trip = model.encoder
+            encoder_trip.eval()
+            encoder_trip.cuda()
+            encoder_trip.logits=False
 
-                    with torch.no_grad():
-                        reps = model(images.cuda()).cpu().numpy()
-                    projected_points = np.concatenate((projected_points, reps))
 
-                projected_points = projected_points[1:]
-                _ = pd.DataFrame(projected_points)
-                _['epsilon'] = epsilon
-                _['model'] = name
-                _['image_index'] = list(range(len(labels)))
-                _['label'] = labels
-                df = pd.concat([df, _])
+            epsilons_dist = np.linspace(start=0, stop=1.0, num=100)
 
-            fcols = df.select_dtypes('float').columns
-            icols = df.select_dtypes('integer').columns
+            encoder_random = CifarResNet18(
+                    embedding_dim=EMBEDDING_DIM, 
+                    hidden_dim=1024,
+                    logits=False,
+                    number_classes=None
+                )
+            encoder_random.load_state_dict(torch.load(f'../multirun/cifar_encoder_random_dim{EMBEDDING_DIM}.pt')) #ensure random always the same
+            encoder_random.eval()
+            encoder_random.cuda()
 
-            df[fcols] = df[fcols].apply(pd.to_numeric, downcast='float')
-            df[icols] = df[icols].apply(pd.to_numeric, downcast='integer')
+            models = [encoder_random, encoder_tripent, encoder_xent,
+                    encoder_ntxent, encoder_trip_sup, encoder_trip]
+            model_names = ['random_init', 'tripent_cifar10', 'xent_cifar10','ntxent_cifar10', 'trip_sup_cifar10', 'trip_cifar10']
 
-            save_loc = f'results/data=cifar10/{OPTIM}/embedding_dim={EMBEDDING_DIM}/white_noise_attacks'
-            if not os.path.exists(save_loc):
-                os.makedirs(save_loc)
-            df.to_pickle(f'{save_loc}/{name}_white_noise_run{confidence_run}.pickle')
+
+
+            transform = torchvision.transforms.Normalize([0.49139968, 0.48215827 ,0.44653124], [0.24703233, 0.24348505, 0.26158768])
+            dataloader = torch.utils.data.DataLoader(test, batch_size=1024, shuffle=False, num_workers=8, persistent_workers=True, pin_memory=False, drop_last=False)
+
             df = pd.DataFrame()
+
+            for model, name in tqdm(zip(models, model_names), total=len(models), desc='model', leave=False):
+                for epsilon in tqdm(epsilons_dist, desc='epsilon', leave=False):
+                    projected_points = np.zeros(shape=(1, EMBEDDING_DIM))
+                    labels = []
+
+                    for batch in tqdm(dataloader, desc='sample', leave=False):
+                        images, labs = batch
+                        labels.extend([int(l) for l in labs])
+                        images = transform(torch.clip(images + torch.normal(0, epsilon, size=images.shape), 0, 1))
+
+                        with torch.no_grad():
+                            reps = model(images.cuda()).cpu().numpy()
+                        projected_points = np.concatenate((projected_points, reps))
+
+                    projected_points = projected_points[1:]
+                    _ = pd.DataFrame(projected_points)
+                    _['epsilon'] = epsilon
+                    _['model'] = name
+                    _['image_index'] = list(range(len(labels)))
+                    _['label'] = labels
+                    df = pd.concat([df, _])
+
+                fcols = df.select_dtypes('float').columns
+                icols = df.select_dtypes('integer').columns
+
+                df[fcols] = df[fcols].apply(pd.to_numeric, downcast='float')
+                df[icols] = df[icols].apply(pd.to_numeric, downcast='integer')
+
+                save_loc = f'../results/data=cifar10/{OPTIM}/embedding_dim={EMBEDDING_DIM}/white_noise_attacks'
+                if not os.path.exists(save_loc):
+                    os.makedirs(save_loc)
+                df.to_pickle(f'{save_loc}/{name}_white_noise_run{0}.pickle')
+                df = pd.DataFrame()
         
